f1b423c71ceee30f19308dc594022356d910187a
Daffa Al Akhdaan <daffaalakhdaan09@gmail.com>
2026-07-11 23:38:19 +0700

    config: add Supabase anon key

--- js/supabase.js
+++ js/supabase.js
@@ -10,7 +10,7 @@
 // =====================================================
 
 const SUPABASE_URL = 'https://vxrgezyfxzynpucuomci.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY_HERE'; // ← Ganti dengan anon key kamu
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4cmdlenlmeHp5bnB1Y3VvbWNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3NzgxNDEsImV4cCI6MjA5OTM1NDE0MX0.3Y9Mal4M76D8fJfcVXQLbPSpLL_m8H7zQ-oVQG6e5IA';
 
 // Initialize Supabase Client
 const { createClient } = supabase;
